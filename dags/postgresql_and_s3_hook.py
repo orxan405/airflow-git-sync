d869853e0428b33498e8fe39709f24cfda73a5ce
orxan405 <qurbanov_orxan@hotmail.com>
2025-04-03 19:00:58 +0400

    tttt

--- dags/postgresql_and_s3_hook.py
+++ dags/postgresql_and_s3_hook.py
@@ -32,7 +32,7 @@ with DAG('s3_to_postgres_dag', default_args=default_args, schedule_interval='@on
     task_id="df_to_s3",
     command=f"""source /dataops/airflowenv/bin/activate && 
     python /dataops/load_df_to_s3.py -ep {endpoint_url} -aki {aws_access_key_id} -sak {aws_secret_access_key} -sfu {source_file_url}""",
-    ssh_conn_id='spark_ssh_conn')
+    ssh_conn_id='spark_ssh_conn')  
 
     # t2 = SSHOperator(task_id='s3_to_postgres', 
     #                 command="""source /dataops/airflowenv/bin/activate &&
